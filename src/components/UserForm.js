b4078300fd5c835adffd69f6c16c3c619edd7cac
Juju <jujuchu@pixnet.tw>
2022-02-13 16:01:57 +0800

    feat: 使用 styled-components 讓提示文字套用紅色

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import useUserForm from '../hooks/useUserForm'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    margin: 0 auto;
+
+    .errors {
+        color: red;
+    }
+`
 
 export default function UserForm() {
     const userForm = useUserForm
@@ -24,20 +36,20 @@ export default function UserForm() {
     })
 
     return (
-        <>
+        <Wrapper>
             <input
                 name="account"
                 onChange={handleChange}
                 value={values.account}
                 placeholder="Account"
-            />{errors.account && (<div>{errors.account}</div>)}
+            />{errors.account && (<div className="errors">{errors.account}</div>)}
 
             <input
                 name="password"
                 onChange={handleChange}
                 value={values.password}
                 placeholder="password"
-            />{errors.password && (<div>{errors.password}</div>)}
+            />{errors.password && (<div className="errors">{errors.password}</div>)}
 
             <label>
                 <input
@@ -48,6 +60,6 @@ export default function UserForm() {
                 />Remember Me
             </label>
             <button onClick={handleSubmit}>Login</button>
-        </>
+        </Wrapper>
     )
 }
