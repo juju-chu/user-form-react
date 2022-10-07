f48fd8016dc061c39bc554f78063254436d60ce0
Juju <jujuchu@pixnet.tw>
2022-10-07 16:55:59 +0800

    test: 觀察生命週期 test

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import useUserForm from '../hooks/useUserForm'
 import styled from 'styled-components';
 
@@ -13,53 +13,75 @@ const Wrapper = styled.div`
     }
 `
 
+const Child = () => {
+    console.log('Child Render');
+    const [name, updateName] = useState('Foo');
+
+    useEffect(() =>{
+        console.log('Child useEffect');
+        updateName('Tom');
+        return () => {
+            console.log('Child Unmount');
+        }
+    }, [name]);
+    console.log(name);
+    return <div>Hello</div>
+}
+
 export default function UserForm() {
-    const userForm = useUserForm
+    console.log('Parent Render');
+    // const userForm = useUserForm
+
+    useEffect(() => {
+        console.log('Parent useEffect');
+    }, [])
+
+    return <Child />;
 
     // 登入畫面範例
-    const { handleChange, handleSubmit, values, errors } = userForm({
-        initialValues: {
-            account: '',
-            password: '',
-            rememberMe: false,
-        },
-        validation: (values) => {
-            const errors = {}
-            if (!values.account) {
-                errors.account = '請輸入帳號'
-            } else if (!values.password) {
-                errors.password = '請輸入密碼'
-            }
-            return errors
-        },
-        onSubmit: (values) => console.table(values),
-    })
+    // const { handleChange, handleSubmit, values, errors } = userForm({
+    //     initialValues: {
+    //         account: '',
+    //         password: '',
+    //         rememberMe: false,
+    //     },
+    //     validation: (values) => {
+    //         const errors = {}
+    //         if (!values.account) {
+    //             errors.account = '請輸入帳號'
+    //         } else if (!values.password) {
+    //             errors.password = '請輸入密碼'
+    //         }
+    //         return errors
+    //     },
+    //     onSubmit: (values) => console.table(values),
+    // })
 
-    return (
-        <Wrapper>
-            <input
-                name="account"
-                onChange={handleChange}
-                value={values.account}
-                placeholder="Account"
-            />{errors.account && (<div className="errors">{errors.account}</div>)}
+    // return (
+    //     <Wrapper>
+    //         <input
+    //             name="account"
+    //             onChange={handleChange}
+    //             value={values.account}
+    //             placeholder="Account"
+    //         />{errors.account && (<div className="errors">{errors.account}</div>)}
 
-            <input
-                name="password"
-                onChange={handleChange}
-                value={values.password}
-                placeholder="password"
-            />{errors.password && (<div className="errors">{errors.password}</div>)}
+    //         <input
+    //             name="password"
+    //             onChange={handleChange}
+    //             value={values.password}
+    //             placeholder="password"
+    //         />{errors.password && (<div className="errors">{errors.password}</div>)}
 
-            <label>
-                <input
-                    type="checkbox"
-                    name="rememberMe"
-                    onChange={handleChange}
-                    checked={values.rememberMe}
-                />Remember Me
-            </label>
-            <button onClick={handleSubmit}>Login</button>
-        </Wrapper>
-    )
+    //         <label>
+    //             <input
+    //                 type="checkbox"
+    //                 name="rememberMe"
+    //                 onChange={handleChange}
+    //                 checked={values.rememberMe}
+    //             />Remember Me
+    //         </label>
+    //         <button onClick={handleSubmit}>Login</button>
+    //     </Wrapper>
+    // )
 }
